fe89bebe137d65dd9a98b95eb9e0356f208d6b62
caiqian <zero530@126.com>
2024-03-29 14:47:53 +0800

    impl server and client

--- ModbusSlaveSimulatorConApp/Program.cs
+++ ModbusSlaveSimulatorConApp/Program.cs
@@ -1,52 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Net;
+
 using System.Net.Sockets;
 using ModbusSlaveSimulatorConApp.Tools;
 using NModbus;
 
-Console.WriteLine("Hello, modbus slave simulator!");
-
-const int port = 502;
-const ushort startAddress = 0;
-// IPAddress address = new IPAddress(new byte[] { 127, 0, 0, 1 });
-
-// create and start the TCP slave
-var slaveTcpListener = new TcpListener(IPAddress.Any, port);
-slaveTcpListener.Start();
-
-var factory = new ModbusFactory();
-
-IModbusSlaveNetwork network = factory.CreateSlaveNetwork(slaveTcpListener);
-
-// var slave1 = factory.CreateSlave(1);
-// var slave2 = factory.CreateSlave(2);
-
-var slaveList = SlaveHelper.CreateSlaves(factory, 2);
-
-foreach (var slave in slaveList)
+var serverTask =Task.Run(() =>
 {
-    // add slaves to network
-    network.AddSlave(slave);
+    ModbusSlaveSimulator.Run();
+    return Task.CompletedTask;
+});
 
-    // create data store
-    var writeDataStore = slave.DataStore;
-    // writeDataStore.SyncRoot.EnterWriteLock();
-    try
-    {
-        var writeData = DataStoreHelper.CreateRandomData();
-        writeDataStore.HoldingRegisters.WritePoints(startAddress, writeData);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-}
+// 等待一段时间，以确保Modbus Slave服务器已经启动
+await Task.Delay(1000);
 
-
+// 启动Modbus客户端
 // 间隔1秒钟，写入寄存器
-var task = Task.Run(async () =>
+var clientTask = Task.Run(async () =>
 {
+    var factory = new ModbusFactory();
+
     while (true)
     {
         // 在这里编写需要每秒执行的代码
@@ -65,25 +38,39 @@ var task = Task.Run(async () =>
 
         var writeData = DataStoreHelper.CreateRandomData(numInputs);
 
-        await master.WriteMultipleRegistersAsync(slaveId, startAddress, writeData);
+        await master.WriteMultipleRegistersAsync(slaveId, 1, writeData);
     }
 });
 
-network.ListenAsync().GetAwaiter().GetResult();
+// 等待用户按下任意键来停止执行
+Console.WriteLine("Press any key to stop.");
+
+Console.ReadKey();
+
+// 停止Modbus Slave服务器
+// TODO: 停止Modbus Slave服务器的逻辑
+
+// 等待任务完成
+await Task.WhenAll(serverTask, clientTask);
+
+Console.WriteLine("Execution stopped.");
+
 
 Console.WriteLine("modbus slave simulator now running!");
 
 
-// 等待用户按下任意键来停止执行
-Console.ReadKey();
-
-// 取消任务并等待完成
-var cts = new CancellationTokenSource();
-cts.Cancel();
-await task;
-
-Console.WriteLine("Code execution stopped.");
-
-
-// prevent the main thread from exiting
-Thread.Sleep(Timeout.Infinite);
+//
+// network.ListenAsync().GetAwaiter().GetResult();
+//
+// Console.WriteLine("modbus slave simulator now running!");
+//
+//
+// // 等待用户按下任意键来停止执行
+// Console.ReadKey();
+//
+// // 取消任务并等待完成
+// var cts = new CancellationTokenSource();
+// cts.Cancel();
+// await task;
+//
+// Console.WriteLine("Code execution stopped.");
---
+++ ModbusSlaveSimulatorConApp/Tools/ModbusSlaveSimulator.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using NModbus;
+
+namespace ModbusSlaveSimulatorConApp.Tools;
+
+public static class ModbusSlaveSimulator
+{
+    public static void Run()
+    {
+        Console.WriteLine("Hello, modbus slave simulator!");
+
+        const int port = 502;
+        const ushort startAddress = 0;
+        // IPAddress address = new IPAddress(new byte[] { 127, 0, 0, 1 });
+
+        // create and start the TCP slave
+        var slaveTcpListener = new TcpListener(IPAddress.Any, port);
+        slaveTcpListener.Start();
+
+        var factory = new ModbusFactory();
+
+        IModbusSlaveNetwork network = factory.CreateSlaveNetwork(slaveTcpListener);
+
+        // var slave1 = factory.CreateSlave(1);
+        // var slave2 = factory.CreateSlave(2);
+
+        var slaveList = SlaveHelper.CreateSlaves(factory, 2);
+
+        foreach (var slave in slaveList)
+        {
+            // add slaves to network
+            network.AddSlave(slave);
+
+            // create data store
+            var writeDataStore = slave.DataStore;
+            // writeDataStore.SyncRoot.EnterWriteLock();
+            try
+            {
+                var writeData = DataStoreHelper.CreateRandomData();
+                writeDataStore.HoldingRegisters.WritePoints(startAddress, writeData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        network.ListenAsync().GetAwaiter().GetResult();
+
+        // prevent the main thread from exiting
+        // Thread.Sleep(Timeout.Infinite);
+    }
+}
